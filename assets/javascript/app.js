0ed7f9c56af66bd19c5d6f54fafb9626a5aded51
atiftariq786 <atiftariq786@hotmail.com>
2019-02-28 20:14:20 -0800

    Modified indux.html quiz.html result.html and app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,59 @@
 
 
 
+$(document).ready (function  (){
 
 
-//$(document).ready(function() {
 
     var number = 15;
     var intervalId ;
     
+//-----------------------Button click section ---------------------------------------------------
 
+        $(document).on("click", ".startQuiz", function(){
+
+            window.location.href = "./quiz.html";
+            
+            timerRun();
+            decrement();
+
+        })
+//------------------------ Timer Section------------------------------------------------------------
         function timerRun() {
-        clearInterval(intervalId);
-        intervalId = setInterval(decrement, 1000);
+            clearInterval(intervalId);
+            intervalId = setInterval(decrement, 1000);
         }
-        timerRun();
+            timerRun();
   
+
         function decrement() {
 
-        number --;
+            number --;
     
-        $("#remainingTime").html(number);
+            $("#remainingTime").html(number);
         
-        if(number === 0){
-            stop();   
-         }
+            if(number === 0){
+                stop();  
+                window.location.href = "./result.html";
+            }
 
    
-    } 
-         timerRun();
-
-         function stop() {
+        } 
+         
+        function stop() {
 
             clearInterval(intervalId);
-          }
+        }
 
-//});
+//------------------Quiz Done Section------------------------------------
+        $(document).on("click", ".finishQuiz", function(){
+
+            window.location.href = "./result.html";
+            
+            stop()
+
+        })
+
+
+
+});
